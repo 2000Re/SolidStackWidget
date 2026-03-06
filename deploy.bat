d5848cc7804acc4a5930bc77bf9b3efef930df3f
invader1978 <akrfjtakrfjt@gmail.com>
2026-03-07 01:25:29 +0900

    Auto deploy

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,8 @@
 setlocal EnableExtensions EnableDelayedExpansion
 cd /d "%~dp0"
 
-:: --------------------------------------------------
-:: 設定エリア
-:: --------------------------------------------------
 set "EXPECTED_ORIGIN=git@github.com:2000Re/MosaicFocusTimer.git"
-:: デプロイ先のブランチ名をここに指定してください
-set "TARGET_BRANCH=branch"
-:: --------------------------------------------------
+set "TARGET_BRANCH=main"
 
 for /f "delims=" %%U in ('git remote get-url origin 2^>nul') do set "ORIGIN=%%U"
 if /I not "%ORIGIN%"=="%EXPECTED_ORIGIN%" (
@@ -19,13 +14,11 @@ if /I not "%ORIGIN%"=="%EXPECTED_ORIGIN%" (
   exit /b 1
 )
 
-:: ファイルの追加とコミット
-git add index.html
-git commit -m "Update index.html"
+git add .
+git commit -m "Auto deploy"
 
-:: 強制プッシュ (-f) を使用して指定ブランチへデプロイ
-echo Force deploying to %TARGET_BRANCH%...
-git push -f origin HEAD:%TARGET_BRANCH% 
+echo Deploying to %TARGET_BRANCH%...
+git push -f origin HEAD:%TARGET_BRANCH%
 
 if %ERRORLEVEL% equ 0 (
     echo [SUCCESS] Deployment complete.
